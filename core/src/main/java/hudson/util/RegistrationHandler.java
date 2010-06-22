da7d1ef83a582557212e8b501d41c8dbe43c82f9
Kedar Mhaswade <kedar.mhaswade@gmail.com>
2010-06-21 22:49:08 -0700

    intermediate step, again

--- core/src/main/java/hudson/util/RegistrationHandler.java
+++ core/src/main/java/hudson/util/RegistrationHandler.java
@@ -1,15 +1,18 @@
 package hudson.util;
 
+import hudson.Util;
+import hudson.model.Hudson;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+import java.security.SecureRandom;
+import static javax.servlet.http.HttpServletResponse.*;
 
 /** Handles Registration. Could be modified to be discoverable.
  * @author Kedar Mhaswade (km@infradna.com)
@@ -17,12 +20,10 @@ import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
  */
 public final class RegistrationHandler {
 
-    private final List<String> methodNames = new ArrayList<String>();
-    private volatile boolean registered = true;
+    private volatile boolean registered = false;
     private final ServletContext context;
-    private final String defaultRegistrationMethod;
-
     private static RegistrationHandler instance;
+    private static final File LICENSE_FILE = new File(Hudson.getInstance().getRootDir(),"license.key");
 
     public synchronized static RegistrationHandler instance(ServletContext context) {
         instance = new RegistrationHandler(context);
@@ -31,9 +32,6 @@ public final class RegistrationHandler {
 
     private RegistrationHandler(ServletContext context) {
         this.context = context;
-        methodNames.add("SERVER_GENERATED");
-        methodNames.add("MANUAL");
-        defaultRegistrationMethod = methodNames.get(0);
         //if (keyFound)
         //registered = true;
     }
@@ -43,16 +41,58 @@ public final class RegistrationHandler {
         return d;
     }
 
-    public List<String> getMethodNames() {
-        return methodNames;
-    }
-
-    public String getDefaultRegistrationMethod() {
-        return defaultRegistrationMethod;
+    public boolean isLicenseKeyValid() {
+        return LICENSE_FILE.exists();    //need a better way
     }
     
     public void doDynamic(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException, InterruptedException {
-        rsp.setStatus(SC_SERVICE_UNAVAILABLE);
-        req.getView(this,"index.jelly").forward(req,rsp);
+        rsp.setStatus(SC_UNAUTHORIZED);
+        rsp.forward(this, "index", req);
+    }
+    
+    public void doSubmit(StaplerRequest request, StaplerResponse response, @QueryParameter String licensingMethod,
+                         @QueryParameter String userName, @QueryParameter String password, @QueryParameter String email,
+                         @QueryParameter String company, @QueryParameter String subscribe,
+                         @QueryParameter String licenseKey) throws IOException, ServletException {
+        //user has clicked on register button
+        if (isServerGenerated(licensingMethod)) {
+            JSONObject j = toRegistrationData(userName, password, email, company, subscribe);
+        } else {
+            if (!verified(licenseKey)) {
+                request.setAttribute("message", "Invalid License Key, try again"); //i18n
+                response.forward(this, "index", request);
+            }
+        }
+        
+    }
+
+    public boolean verified(String licenseKey) {
+        return false; //for now, just make them go the server_generated route
+    }
+
+    private JSONObject toRegistrationData(String userName, String password, String email, String company, String subscribe) {
+        JSONObject j = new JSONObject();
+        j.put("userName", userName);
+        j.put("password", password);
+        j.put("email", email);
+        j.put("company", company);
+        j.put("subscribe", subscribe);
+        j.put("instance-id", Util.getDigestOf(Hudson.getInstance().getSecretKey()));
+        return j;
+    }
+
+    private static boolean isServerGenerated(String licensingMethod) {
+        return "1".equals(licensingMethod);
+    }
+
+    private void writeLicenseKey(String licenseKey) {
+        try {
+            TextFile secretFile = new TextFile(LICENSE_FILE);
+            if(!secretFile.exists()) {
+                secretFile.write(licenseKey);
+            }
+        } catch(IOException e) {
+            //ignore?
+        }
     }
 }
