f62c81522921e6a04e6c83c7951853e929f43226
Kedar Mhaswade <Kedar.Mhaswade@gmail.com>
2010-07-02 18:37:49 -0700

    Moved the @Extension annotation on the public static getInstance method from the Reminder class itself. This resolves the problem arising from two instances of Reminder.

--- core/src/main/java/hudson/license/Reminder.java
+++ core/src/main/java/hudson/license/Reminder.java
@@ -2,9 +2,18 @@ package hudson.license;
 
 import hudson.Extension;
 import hudson.ExtensionList;
+import hudson.ExtensionPoint;
+import hudson.model.Describable;
 import hudson.model.ManagementLink;
 import hudson.model.PageDecorator;
 import hudson.util.TimeUnit2;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Implements the functionality to remind (nag) the users to buy a support license.
@@ -12,30 +21,58 @@ import hudson.util.TimeUnit2;
  * @author Kedar Mhaswade (km@infradna.com)
  *         Date: Jun 30, 2010
  */
-@Extension
 public class Reminder extends PageDecorator {
 
     private volatile LicenseManager lm;
     private volatile long lastNagTime;
-    public Reminder() {
+    private volatile long lastReminderTime;
+    private final AtomicBoolean remind = new AtomicBoolean(true);
+    private static Reminder theInstance; //guarded by synchronized method
+    private Reminder() {
         super(Reminder.class);
     }
 
+    @Extension
+    public static synchronized Reminder getInstance() {
+        if (theInstance != null)
+            return theInstance;
+        theInstance = new Reminder();
+        return theInstance;
+    }
+    
     public boolean isDue() {
+        if (remind.get() == false)
+            return false;
         findLicenseManager();
         long last = lastNagTime;
         long now  = System.currentTimeMillis();
         boolean nag = false;
-        if (TimeUnit2.MILLISECONDS.toDays(now-last) > 1) {
+        if (TimeUnit2.MILLISECONDS.toDays(now-last) > 2 || Boolean.getBoolean("LicenseExpired")) {
             nag = true;
             lastNagTime = now;
         }
-        if (lm.getRemainingDays() <= 60 && nag) { //getting nagged is NOT optional, you will get nagged daily :-)
+        if (lm.getRemainingDays() <= 60 && nag) {
             return true;
         }
         return false;
     }
 
+    public void doAct(StaplerRequest request, StaplerResponse response, @QueryParameter (fixEmpty = true) String yes,
+                      @QueryParameter (fixEmpty = true) String no) throws IOException, ServletException {
+        if (yes != null) {
+            response.sendRedirect("http://license.infradna.com/new");
+        } else if (no != null) {
+            remind.set(false);
+            response.forwardToPreviousPage(request);
+        } else { //remind later
+            lastNagTime = System.currentTimeMillis();
+            response.forwardToPreviousPage(request);
+        }
+    }
+
+    public void doDynamic(StaplerRequest request, StaplerResponse response) {
+        System.out.println(request);
+    }
     private void findLicenseManager() {
         if (lm == null) {
             ExtensionList<ManagementLink> lms = LicenseManager.all();
