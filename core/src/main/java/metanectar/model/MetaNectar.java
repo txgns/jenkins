d7364be09ea284394c3cb6117bf331aaae19ba99
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-03-05 17:15:51 -0800

    setup the initial view state to the one that suits Meta Nectar

--- core/src/main/java/metanectar/model/MetaNectar.java
+++ core/src/main/java/metanectar/model/MetaNectar.java
@@ -2,11 +2,17 @@ package metanectar.model;
 
 import hudson.PluginManager;
 import hudson.model.Hudson;
+import hudson.model.ListView;
+import hudson.model.View;
+import hudson.views.JobColumn;
+import hudson.views.StatusColumn;
 import org.jvnet.hudson.reactor.ReactorException;
 
+import javax.print.attribute.standard.JobName;
 import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @author Paul Sandoz
@@ -25,4 +31,16 @@ public class MetaNectar extends Hudson {
         return "MetaNectar";
     }
 
+    /**
+     * Sets up the initial view state.
+     */
+    @Override
+    protected View createInitialView() {
+        ListView lv = new ListView("All");
+        lv.setColumns(Arrays.asList(
+                new StatusColumn(),
+                new JobColumn()
+        ));
+        return lv;
+    }
 }
