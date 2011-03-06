414b001089fa74382c9ea585fdccc5b6d22e0fcf
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-03-05 16:54:01 -0800

    added the Jenkins server representation

---
+++ core/src/main/java/metanectar/model/JenkinsServer.java
@@ -0,0 +1,52 @@
+package metanectar.model;
+
+import hudson.Extension;
+import hudson.model.AbstractItem;
+import hudson.model.Hudson;
+import hudson.model.ItemGroup;
+import hudson.model.Job;
+import hudson.model.TopLevelItem;
+import hudson.model.TopLevelItemDescriptor;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Representation of remote Jenkins server inside Meta Nectar.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class JenkinsServer extends AbstractItem implements TopLevelItem {
+    
+    protected JenkinsServer(ItemGroup parent, String name) {
+        super(parent, name);
+    }
+
+    /**
+     * No nested job under Jenkins server
+     *
+     * @deprecated
+     *      No one shouldn't be calling this directly.
+     */
+    @Override
+    public Collection<? extends Job> getAllJobs() {
+        return Collections.emptyList();
+    }
+
+    public TopLevelItemDescriptor getDescriptor() {
+        return (TopLevelItemDescriptor) Hudson.getInstance().getDescriptorOrDie(getClass());
+    }
+
+    @Extension
+    public static class DescriptorImpl extends TopLevelItemDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Jenkins server";
+        }
+
+        @Override
+        public TopLevelItem newInstance(ItemGroup parent, String name) {
+            return new JenkinsServer(parent,name);
+        }
+    }
+}
