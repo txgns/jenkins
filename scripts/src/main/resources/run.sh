8cc7556442eaa139cb72426f20635c3809e16bde
Stephen Connolly <stephen.alan.connolly@gmail.com>
2011-05-04 12:00:12 +0100

    simpilfy testing meta-nectar

---
+++ scripts/src/main/resources/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WORK="$(dirname "$0")"
+
+source "${WORK}/run.conf"
+
+CURDIR="$(pwd)"
+cd "${WORK}/scripts"
+chmod +x *.sh
+
+cd "${JENKINS_HOME}"
+java "-DMETANECTAR_PROPERTIES_URL=${METANECTAR_PROPERTIES_URL}" \
+  -Dmetanectar.provisioning.MasterProvisioner.initialDelay=10000 \
+  -Dmetanectar.provisioning.MasterProvisioner.recurrencePeriod=5000 \
+  "-DJENKINS_HOME=${JENKINS_HOME}" \
+   -jar "${CURDIR}/${WORK}/metanectar-war.war" $*
+cd "${CURDIR}"
