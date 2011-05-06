919193e1aa8e9f9763ae859ca7adf6c7a0b80b89
Paul Sandoz <paul.d.sandoz@googlemail.com>
2011-05-06 17:13:12 +0200

    Use png instead of gif where possible. Support Location header on MN agent URL for root URL of MN.

--- core/src/main/java/metanectar/model/MasterServer.java
+++ core/src/main/java/metanectar/model/MasterServer.java
@@ -79,8 +79,8 @@ public class MasterServer extends AbstractItem implements TopLevelItem, HttpResp
         Provision("new-computer.png"),
         Start("start-computer.png"),
         Stop("stop-computer.png"),
-        Terminate("edit-delete.gif"),
-        Delete("edit-delete.gif");
+        Terminate("edit-delete.png"),
+        Delete("edit-delete.png");
 
         public final String icon;
 
