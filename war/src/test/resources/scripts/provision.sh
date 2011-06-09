19c82400d28729cc7ddd2f64b9a9b82dd347c68e
Stephen Connolly <stephen.alan.connolly@gmail.com>
2011-06-09 12:54:45 +0100

    add non-blocking debugging of provisioned masters (starting with port 5005

--- war/src/test/resources/scripts/provision.sh
+++ war/src/test/resources/scripts/provision.sh
@@ -2,7 +2,7 @@
 
 WORK="$(dirname "$0")"
 
-MASTER_HOME=${MASTER_HOME_LOCATION}/${MASTER_INDEX}-${MASTER_NAME}
+MASTER_HOME="${MASTER_HOME_LOCATION}/${MASTER_INDEX}-${MASTER_NAME}"
 export MASTER_HOME
 if [ ! -d "${MASTER_HOME}" ]
 then
@@ -18,7 +18,7 @@ then
   find "${WORK}" -name \*.hpi -exec cp "{}" "${MASTER_HOME}/plugins/" \;
 
   echo "cd \"${MASTER_HOME}\";" > "${MASTER_HOME}.start"
-  echo "java \"-DMASTER_ENDPOINT=http://localhost:${MASTER_PORT}\" \"-DJENKINS_HOME=${MASTER_HOME}\" \"-DMASTER_INDEX=${MASTER_INDEX}\" \"-DMASTER_NAME=${MASTER_NAME}\" \"-DMASTER_METANECTAR_ENDPOINT=${MASTER_METANECTAR_ENDPOINT}\" \"-DMASTER_GRANT_ID=${MASTER_GRANT_ID}\" -jar "${WORK}/jenkins-war.war" --httpPort=${MASTER_PORT} --ajp13Port=-1 &> \"${MASTER_HOME}.log.txt\" &" >> "${MASTER_HOME}.start"
+  echo "java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=$((${MASTER_PORT}-9090+5005)) \"-DMASTER_ENDPOINT=http://localhost:${MASTER_PORT}\" \"-DJENKINS_HOME=${MASTER_HOME}\" \"-DMASTER_INDEX=${MASTER_INDEX}\" \"-DMASTER_NAME=${MASTER_NAME}\" \"-DMASTER_METANECTAR_ENDPOINT=${MASTER_METANECTAR_ENDPOINT}\" \"-DMASTER_GRANT_ID=${MASTER_GRANT_ID}\" -jar "${WORK}/jenkins-war.war" --httpPort=${MASTER_PORT} --ajp13Port=-1 &> \"${MASTER_HOME}.log.txt\" &" >> "${MASTER_HOME}.start"
   echo 'echo $! > ${MASTER_HOME}.pid && cd -' >> ${MASTER_HOME}.start
 fi
 
