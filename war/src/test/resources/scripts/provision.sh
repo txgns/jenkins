200c844e7ea6ff52480948a2be779a0daef0b56e
Stephen Connolly <stephen.alan.connolly@gmail.com>
2011-06-09 12:28:13 +0100

    remove scripts module and move to war module.
    to run a demo you now use "mvn -Prun" faster is "mvn -Prun -pl war" and if you want to rebuild dependencies "mvn -Prun -pl war -am"

--- war/src/test/resources/scripts/provision.sh
+++ war/src/test/resources/scripts/provision.sh
@@ -15,10 +15,10 @@ then
     unzip -q ${file} -d "${MASTER_HOME}" 1>&2
   fi
 
-  find "${WORK}/../" -name \*.hpi -exec cp "{}" "${MASTER_HOME}/plugins/" \;
+  find "${WORK}" -name \*.hpi -exec cp "{}" "${MASTER_HOME}/plugins/" \;
 
   echo "cd \"${MASTER_HOME}\";" > "${MASTER_HOME}.start"
-  echo "java \"-DMASTER_ENDPOINT=http://localhost:${MASTER_PORT}\" \"-DJENKINS_HOME=${MASTER_HOME}\" \"-DMASTER_INDEX=${MASTER_INDEX}\" \"-DMASTER_NAME=${MASTER_NAME}\" \"-DMASTER_METANECTAR_ENDPOINT=${MASTER_METANECTAR_ENDPOINT}\" \"-DMASTER_GRANT_ID=${MASTER_GRANT_ID}\" -jar "${WORK}/../jenkins-war.war" --httpPort=${MASTER_PORT} --ajp13Port=-1 &> \"${MASTER_HOME}.log.txt\" &" >> "${MASTER_HOME}.start"
+  echo "java \"-DMASTER_ENDPOINT=http://localhost:${MASTER_PORT}\" \"-DJENKINS_HOME=${MASTER_HOME}\" \"-DMASTER_INDEX=${MASTER_INDEX}\" \"-DMASTER_NAME=${MASTER_NAME}\" \"-DMASTER_METANECTAR_ENDPOINT=${MASTER_METANECTAR_ENDPOINT}\" \"-DMASTER_GRANT_ID=${MASTER_GRANT_ID}\" -jar "${WORK}/jenkins-war.war" --httpPort=${MASTER_PORT} --ajp13Port=-1 &> \"${MASTER_HOME}.log.txt\" &" >> "${MASTER_HOME}.start"
   echo 'echo $! > ${MASTER_HOME}.pid && cd -' >> ${MASTER_HOME}.start
 fi
 
