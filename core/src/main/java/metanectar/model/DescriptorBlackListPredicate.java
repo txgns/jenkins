c352ca8a6f586050b1cdbfd1b4d8faf1cf8bf966
Stephen Connolly <stephen.alan.connolly@gmail.com>
2011-06-10 10:05:14 +0100

    scan the plugin classpath for contributed black-lists

--- core/src/main/java/metanectar/model/DescriptorBlackListPredicate.java
+++ core/src/main/java/metanectar/model/DescriptorBlackListPredicate.java
@@ -4,11 +4,12 @@ import com.google.common.base.Predicate;
 import com.sun.istack.internal.Nullable;
 import hudson.model.Describable;
 import hudson.model.Descriptor;
-import hudson.slaves.ComputerLauncher;
+import hudson.model.Hudson;
 
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
@@ -26,32 +27,44 @@ public class DescriptorBlackListPredicate<T extends Describable<T>> implements P
     protected DescriptorBlackListPredicate() {
         blackList = new HashSet<String>();
 
-        URL resource = getClass().getResource(getClass().getSimpleName() + ".exclude");
-        if (resource != null) {
-            try {
-                for (String line : com.google.common.io.Resources.readLines(resource, Charset.forName("UTF-8"))) {
-                    int i = line.indexOf('#');
-                    if (i != -1) {
-                        line = line.substring(0, i);
-                    }
-                    line = line.trim();
-                    if (!line.isEmpty()) {
-                        blackList.add(line);
+        try {
+            Enumeration<URL> resources = Hudson.getInstance().pluginManager.uberClassLoader
+                    .getResources(getClass().getSimpleName() + ".exclude");
+            while (resources.hasMoreElements()) {
+                URL resource = resources.nextElement();
+                if (resource != null) {
+                    try {
+                        for (String line : com.google.common.io.Resources
+                                .readLines(resource, Charset.forName("UTF-8"))) {
+                            int i = line.indexOf('#');
+                            if (i != -1) {
+                                line = line.substring(0, i);
+                            }
+                            line = line.trim();
+                            if (!line.isEmpty()) {
+                                blackList.add(line);
+                            }
+                        }
+                    } catch (IOException e) {
+                        LogRecord r = new LogRecord(Level.WARNING, "Could not read {0}'s black-list from {1}");
+                        r.setThrown(e);
+                        r.setParameters(new Object[]{getClass().getSimpleName(), resource});
+                        Logger.getLogger(getClass().getName()).log(r);
                     }
                 }
-            } catch (IOException e) {
-                LogRecord r = new LogRecord(Level.WARNING, "Could not read {0}'s black-list");
-                r.setThrown(e);
-                r.setParameters(new Object[]{getClass().getSimpleName()});
-                Logger.getLogger(getClass().getName()).log(r);
             }
+        } catch (IOException e) {
+            LogRecord r = new LogRecord(Level.WARNING, "Could not read {0}'s black-lists");
+            r.setThrown(e);
+            r.setParameters(new Object[]{getClass().getSimpleName()});
+            Logger.getLogger(getClass().getName()).log(r);
         }
     }
 
     /**
      * {@inheritDoc}
      */
-    public final boolean apply(@Nullable Descriptor<T> input) {
+    public boolean apply(@Nullable Descriptor<T> input) {
         return input != null && !blackList.contains(input.clazz.getName());
     }
 }
