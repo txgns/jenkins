99f1ea7b14118248a5314fa4f0b639cc2ae0b3d0
Ryan Campbell <ryan.campbell@gmail.com>
2012-11-13 10:39:17 -0600

    Bogus import

--- core/src/main/java/hudson/model/Run.java
+++ core/src/main/java/hudson/model/Run.java
@@ -97,7 +97,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 
 import jenkins.model.Jenkins;
-import jenkins.util.io.OnMaster;
 
 import net.sf.json.JSONObject;
 import org.apache.commons.io.input.NullInputStream;
