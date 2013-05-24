6a32ce3e80032046bae43442760478c59ef565b6
Jesse Glick <jglick@cloudbees.com>
2013-05-24 12:06:37 -0400

    [FIXED JENKINS-18074] Avoid clearing AbstractItem.displayName when the new parent ItemGroup is different. (cherry picked from commit 04e2ccade6c831578bdd32b5e5982cf150f635f4)
    Conflicts:
    	changelog.html
    	test/src/test/java/hudson/model/DisplayNameTest.java
    
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

--- test/src/test/java/hudson/model/DisplayNameTest.java
+++ test/src/test/java/hudson/model/DisplayNameTest.java
@@ -28,6 +28,9 @@ import jenkins.model.Jenkins;
 import org.junit.Assert;
 import org.junit.Test;
 import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.Bug;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.MockFolder;
 
 /**
  * @author kingfai
@@ -87,4 +90,16 @@ public class DisplayNameTest extends HudsonTestCase {
         Assert.assertEquals(newProjectName, newProject.getDisplayName());
         
     }
+
+    @Bug(18074)
+    @Test public void copyJobWithDisplayNameToDifferentFolder() throws Exception {
+        MockFolder d1 = j.createFolder("d1");
+        FreeStyleProject job = d1.createProject(FreeStyleProject.class, "job");
+        job.setDisplayName("My Job");
+        MockFolder d2 = j.jenkins.copy(d1, "d2");
+        FreeStyleProject j2 = (FreeStyleProject) d2.getItem("job");
+        Assert.assertNotNull(j2);
+        Assert.assertEquals("My Job", j2.getDisplayName());
+    }
+
 }
