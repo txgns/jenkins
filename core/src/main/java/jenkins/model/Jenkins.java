90f0cc8c51951c88cf4d1b18356359b4ecefd79b
Ryan Campbell <ryan.campbell@gmail.com>
2013-07-09 09:56:44 -0700

    Prevent users from editng the Jenkins workspaceDir & buildsDir

--- core/src/main/java/jenkins/model/Jenkins.java
+++ core/src/main/java/jenkins/model/Jenkins.java
@@ -2843,9 +2843,6 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
 
             JSONObject json = req.getSubmittedForm();
 
-            workspaceDir = json.getString("rawWorkspaceDir");
-            buildsDir = json.getString("rawBuildsDir");
-
             systemMessage = Util.nullify(req.getParameter("system_message"));
 
             jdks.clear();
