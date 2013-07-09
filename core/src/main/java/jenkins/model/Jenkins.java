7f4b17bfdf3c02d17fb834f259dd837819790524
Ryan Campbell <ryan.campbell@gmail.com>
2013-07-09 09:48:23 -0700

    prevent users from seeing labels of ephemeral slaves

--- core/src/main/java/jenkins/model/Jenkins.java
+++ core/src/main/java/jenkins/model/Jenkins.java
@@ -1609,7 +1609,7 @@ public class Jenkins extends AbstractCIBase implements ModifiableTopLevelItemGro
     public Set<Label> getLabels() {
         Set<Label> r = new TreeSet<Label>();
         for (Label l : labels.values()) {
-            if(!l.isEmpty())
+            if(!l.isEmpty() && ! l.isSelfLabel())
                 r.add(l);
         }
         return r;
